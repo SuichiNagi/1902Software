5fea438fcdcc63df1cac9f66b420202c66251a55
SuichiNagi <aldreiglennkim@gmail.com>
2024-07-18 21:52:13 +0800

    add remember me checkbox and remove some unnecessary message in ErrorMessage

---
+++ 1902Software/Views/SWCheckbox.swift
@@ -0,0 +1,20 @@
+//
+//  SWCheckbox.swift
+//  1902Software
+//
+//  Created by Aldrei Glenn Nuqui on 7/18/24.
+//
+
+import UIKit
+
+class SWCheckbox: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
